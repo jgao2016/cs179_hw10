ad025794fd7c0e84bca62992b20219fab8ea95af
Jingchen <gaojc@mit.edu>
2018-04-10 20:10:53 -0400

    chaning

---
+++ game/js/group_name.js
@@ -0,0 +1,25 @@
+import {state, saveState} from '/js/common.js';
+
+state.session.user=state.users[0];
+saveState(state);
+
+
+function submit(){
+    let content = document.getElementById('group_name').value;
+    document.getElementById('group_name').sour
+    state.session.user.meal_group=content;
+    saveState(state);
+    console.log("content");
+    console.log(content);
+    console.log("state");
+    console.log(state);
+}
+if (state.session.user==null){
+    console.log('redirect to home');
+    window.location =  '/';
+}
+
+document.getElementById("group_name_submit").addEventListener("click", function(){
+    submit();
+}); 
+
