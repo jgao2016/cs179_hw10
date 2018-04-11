61fd63c3a3bb27e4545cb1f6d75c460a75a5b818
Jingchen <gaojc@mit.edu>
2018-04-11 11:01:37 -0400

    more changes to game part

--- game/js/group_start.js
+++ game/js/group_start.js
@@ -3,25 +3,14 @@ import {state, saveState} from '/js/common.js';
 state.session.user=state.users[0];
 saveState(state);
 
-// $('#hour-selector').change(function () {
-//     var selectedText = $(this).find("option:selected").text();
-    
-//     $('#hour-selector').text(selectedText);
-// });
 
-
-// $('#hour-selector').on('show.bs.dropdown', function () {
-//     console.log(selectedText);
-//     console.log("ppp");
-// })
-// console.log("ppp--");
-
-// $('#hour-selector').on('click', function(){
-//     // $('#datebox').val($(this).text());
-//     console.log("ppp");
-// });
-
-$(document).on('click', '.dropdown-menu li a', function() {
-    var selectedText = $('#hour-selector').find("option:selected").text();
-    console.log();
-}); 
+$('#minutelist li').on('click', function(){
+    $('#minute').val($(this).text());
+    console.log($(this).text());
+    console.log("mmm")
+});
+$('#hourlist li').on('click', function(){
+    $('#hour').val($(this).text());
+    console.log($(this).text());
+    console.log("hhh")
+});
