dacf93deffd433342f88213d2e57d11ee2f4602e
Nam Wook Kim <namwkim85@gmail.com>
2018-04-20 00:44:08 -0400

    update friends

--- js/friends.js
+++ js/friends.js
@@ -1,5 +1,6 @@
 document.querySelector(".add-friend").addEventListener("click",function(){
  var FriendName = prompt("Enter your Friends Name","Name");
+ if (FriendName==null) return;
  var row = document.createElement("tr");
  row.innerHTML = `
  <th scope="row">5</th>
